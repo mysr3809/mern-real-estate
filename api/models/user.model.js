51a521bd39b909f0c268d364d9b1222b7b74cb1c
mysr3809 <myasar0938@gmail.com>
2024-01-05 11:00:44 +0100

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userScema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userScema);
+
+export default User;
