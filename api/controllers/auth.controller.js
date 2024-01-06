d5bad6a068293e324ba32d8cd167f974023fd760
mysr3809 <myasar0938@gmail.com>
2024-01-06 23:54:19 +0100

    finished sigi in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,13 +28,14 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, "Invalid credentials"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...others } = validUser._doc;
     res
       .cookie("token", token, {
         httpOnly: true,
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
       })
       .status(200)
-      .json(validUser);
+      .json(others);
   } catch (error) {
     next(error);
   }
