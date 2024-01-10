2b3405b6ac067465b96194c4608d48c6efded3b6
mysr3809 <myasar0938@gmail.com>
2024-01-10 15:48:33 +0100

    update user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,22 +19,15 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) {
-      return next(errorHandler(404, "User not found"));
-    }
+    if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) {
-      return next(errorHandler(401, "Invalid credentials"));
-    }
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: hashedPassword, ...others } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-      })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(others);
+      .json(rest);
   } catch (error) {
     next(error);
   }
