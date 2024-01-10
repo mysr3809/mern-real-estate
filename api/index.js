dacce4fa1a5ceedbb18327a6838d0914b4e73f54
mysr3809 <myasar0938@gmail.com>
2024-01-10 11:51:53 +0100

    create user update functionality

--- api/index.js
+++ api/index.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.listen(3000, () => console.log("Listening on port 3000"));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRoute);
+
 app.use((err, req, res, next) => {
   const statusCode = err.status || 500;
   const message = err.message || "Something went wrong";
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,15 +35,15 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to={"/profile"}>
+          <Link to="/profile">
             {currentUser ? (
               <img
-                src={currentUser?.avatar}
-                className="w-8 h-8 rounded-full"
-                alt={currentUser?.name}
-              ></img>
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
             ) : (
-              <li className=" text-slate-700 hover:underline">Sign In</li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
