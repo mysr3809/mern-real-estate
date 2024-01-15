c98d35c1fb24b40176501bb4e2ab637445cc78a5
mysr3809 <myasar0938@gmail.com>
2024-01-15 12:27:31 +0100

    search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,7 +3,7 @@ import {
   createListing,
   deleteListing,
   updateListing,
-  getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,6 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
