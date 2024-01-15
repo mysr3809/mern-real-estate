2bb6e3b36d2fe95bd704547438d5a43d635c0cb6
mysr3809 <myasar0938@gmail.com>
2024-01-15 12:49:01 +0100

    update header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      console.log(searchTermFromUrl);
+      setSearchTerm(searchTermFromUrl);
+      console.log(searchTerm);
+    }
+  }, [window.location.search]);
   return (
     <header className="bg-slate-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -16,13 +35,20 @@ export default function Header() {
             <span className="text-green-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex justify-between items-center">
+        <form
+          onSubmit={handleSumbit}
+          className="bg-slate-100 p-3 rounded-lg flex justify-between items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
+            value={searchTerm}
             className="bg-transparent text-sm sm:text-lg focus:outline-none w-24 sm:w-64"
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch />
+          <button>
+            <FaSearch />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
